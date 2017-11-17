b8a723110ae66a6423848cc31a689dcf1f33d418
linkydee <32688089+linkydee@users.noreply.github.com>
2017-11-16 18:54:44 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,35 +33,49 @@ const kmsWords = ["B.Kms", "B.kms", "B.KMS"];
 const helpWords = ["B.help", "B.help", "B.HELP", "Boku Help", "Boku help", "boku help", "BOKU HELP"]
 const slimeWords = ["B.KILLSLIME", "B.Killslime", "B.killslime"];
 
-let XP = JSON.parse(fs.readFileSync('./XP.json', 'utf8'));
-
-let userData = XP[msg.author.id];
-if (!userData) userData = {XP: 0, level: 0};
-
-let curLevel = Math.floor(0.1 * Math.sqrt(userXP));
-if (curLevel > userData.level) {
-    userData.level = curLevel;
-    msg.reply('You have lvled ^ to lvl **${curLevel}**!');
-}
-
-console.log("level")
-if (msg.content.startsWith("level")) {
-    msg.reply(`You are lvl ${userData.level}, with ${userData.XP} XP Right Now.`);
-}
-
-if (!XP[msg.author.id]) XP[msg.author.id] = {XP: 0, level: 0}
-
-
-
-console.log("Slime")
-if (msg.content.startsWith(slimeWords)) {
-    userData.XP += 10
-    msg.channel.sendMessage(`${msg.author} has killed a Slime!`)
-}
-
-console.log(XP)
-fs.writeFile('./XP.json', JSON.stringify(XP), console.error);
-
+bot.on("message", msg => {
+	let prefix = "=";
+	
+	console.log(0)
+	if(!msg.content.startsWith(prefix)) return;
+	
+	//console.log(0.1)
+	//if(msg.author.id != "Your ID") return;
+	//Only use the above for testing as only the person with that ID can use the bot.
+	
+	console.log("help command")
+	if (msg.content.startsWith(prefix + "help")) {
+		msg.reply("Here are my commands: !help,")
+	}
+	
+	let userData = XP[msg.author.id];
+	if (!userData) userData = {XP: 0, level: 0};
+	
+	let userXP = XP[msg.author.id] ? XP[msg.author.id].XP : 0;
+	let curLevel = Math.floor(0.1 * Math.sqrt(userXP));
+	if (curLevel > userData.level) {
+		userData.level = curLevel;
+		msg.reply(`You have lvled ^ to lvl **${curLevel}**!`);
+	}
+	
+	console.log("level")
+	if (msg.content.startsWith(prefix + "level")) {
+		msg.reply(`You are lvl ${userData.level}, with ${userData.XP} XP Right Now.`);
+	}
+	
+	if (!XP[msg.author.id]) XP[msg.author.id] = {XP: 0, level: 0}
+	
+	
+	
+	console.log("Example")
+	if (msg.content.startsWith(prefix + "killExample")) {
+		userData.XP += 10
+		msg.channel.sendMessage(`${msg.author} has killed an Example!`)
+	}
+	
+	console.log(XP)
+	fs.writeFile('./XP.json', JSON.stringify(XP), console.error);
+	
 });
 
 client.on("message", (message) => {
