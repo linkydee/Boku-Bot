e0aa472dc824b0fcbc0d7e6c8ba86657152cd9d7
linkydee <32688089+linkydee@users.noreply.github.com>
2017-11-16 19:04:41 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,37 +35,41 @@ const slimeWords = ["B.KILLSLIME", "B.Killslime", "B.killslime"];
 const levelWords = ["B.LEVEL", "B.Level", "B.level"];
 
 bot.on("message", msg => {
-	//console.log(0.1)
-	//if(msg.author.id != "Your ID") return;
-	//Only use the above for testing as only the person with that ID can use the bot.
-	let userData = XP[msg.author.id];
-	if (!userData) userData = {XP: 0, level: 0};
-	
-	let userXP = XP[msg.author.id] ? XP[msg.author.id].XP : 0;
-	let curLevel = Math.floor(0.1 * Math.sqrt(userXP));
-	if (curLevel > userData.level) {
-		userData.level = curLevel;
-		msg.reply(`You have lvled ^ to lvl **${curLevel}**!`);
-	}
-	
-	console.log("level")
-	if (msg.content.startsWith(levelWords)) {
-		msg.reply(`You are lvl ${userData.level}, with ${userData.XP} XP Right Now.`);
-	}
-	
-	if (!XP[msg.author.id]) XP[msg.author.id] = {XP: 0, level: 0}
-	
-	
-	
-	console.log("Example")
-	if (msg.content.startsWith(slimeWords)) {
-		userData.XP += 10
-		msg.channel.sendMessage(`${msg.author} has killed an Slime!`)
-	}
-	
-	console.log(XP)
-	fs.writeFile('./XP.json', JSON.stringify(XP), console.error);
-	
+let prefix = "!";
+
+console.log(0)
+if(!msg.content.startsWith(prefix)) return;
+
+console.log(0.1)
+if(msg.author.id != "235144525763444736") return;
+
+let userData = XP[msg.author.id];
+if (!userData) userData = {XP: 0, level: 0};
+
+let curLevel = Math.floor(0.1 * Math.sqrt(userXP));
+if (curLevel > userData.level) {
+    userData.level = curLevel;
+    msg.reply('You have lvled ^ to lvl **${curLevel}**!');
+}
+
+console.log("level")
+if (msg.content.startsWith(prefix + "level")) {
+    msg.reply(`You are lvl ${userData.level}, with ${userData.XP} XP Right Now.`);
+}
+
+if (!XP[msg.author.id]) XP[msg.author.id] = {XP: 0, level: 0}
+
+
+
+console.log("Slime")
+if (msg.content.startsWith(prefix + "killSlime")) {
+    userData.XP += 10
+    msg.channel.sendMessage(`${msg.author} has killed a Slime!`)
+}
+
+console.log(XP)
+fs.writeFile('./XP.json', JSON.stringify(XP), console.error);
+
 });
 
 client.on("message", (message) => {
