f53d76fa803ce5cc44e910674c1fb66d1cd8dd7b
linkydee <32688089+linkydee@users.noreply.github.com>
2017-12-13 19:01:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,11 +46,7 @@ client.on("message", (message) => {
   }
 }); 
 
-client.on("message", (message) => {
-  if(message.author.id == 208325184556498944) ) {
-    message.channel.send("Person above me is gay!")
-  }
-}); 
+
 
 
 client.on("message", (message) => {
